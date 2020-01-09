8510ce92544d8edb3223b69121de945bcbe40971
Edmund Lui <edmund.lui1998@gmail.com>
2020-01-09 14:34:02 -0500

    first commit

---
+++ luix5540_a1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  luix5540_a1
+//
+//  Created by Prism Student on 2020-01-09.
+//  Copyright © 2020 wlu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ luix5540_a1Tests/luix5540_a1Tests.swift
@@ -0,0 +1,34 @@
+//
+//  luix5540_a1Tests.swift
+//  luix5540_a1Tests
+//
+//  Created by Prism Student on 2020-01-09.
+//  Copyright © 2020 wlu. All rights reserved.
+//
+
+import XCTest
+@testable import luix5540_a1
+
+class luix5540_a1Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
